562a589b2808ded6b3aedc8c07171c277e5cbc25
Fran <franbl98@gmail.com>
2021-11-02 10:21:03 +0100

    Empezando proyecto de carrito de la compra

---
+++ CarritoDeLaCompra/src/main/java/servlets/Users.java
@@ -0,0 +1,23 @@
+package servlets;
+
+import java.util.HashMap;
+
+public class Users {
+    private final HashMap <String,String> users = new HashMap<>();
+    
+    
+    public Users() {
+    	users.put("FRAN","FRANBL");
+    	users.put("USER","USER");
+    	users.put("123","123");
+    	users.put("JORGE","JORGE");
+    	users.put("USUARIO","CONTRASEÑA");
+    	users.put("PRUEBA","PRUEBA");
+    }
+
+
+	public HashMap<String, String> getUsers() {
+		return users;
+	}
+    
+}
